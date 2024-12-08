119174ee0c9f118b9208269e0caff8c9d545c1b9
HoryunLee <ghfusdl7201@gmail.com>
2024-12-08 21:53:13 +0900

    ADD:UserService,Signup

--- src/main/java/gift/controller/SignupController.java
+++ src/main/java/gift/controller/SignupController.java
@@ -1,5 +1,6 @@
 package gift.controller;
 
+
 import gift.model.User;
 import gift.service.UserService;
 import org.springframework.stereotype.Controller;
@@ -26,6 +27,25 @@ public class SignupController {
 
     @PostMapping()
     public String signupUser(@ModelAttribute User user, Model model) {
+        String signupError = null;
+
+        if (!userService.isUsernameAvailable(user.getUsername())) {
+            signupError = "The username already exists.";
+        }
+
+        if (signupError == null) {
+            int rowsAdded = userService.createUser(user);
+            if (rowsAdded < 0) {
+                signupError = "There was an error signing you up. Please try again.";
+            }
+        }
+
+        if (signupError == null) {
+            model.addAttribute("signupSuccess", true);
+        } else {
+            model.addAttribute("signupError", signupError);
+        }
+
         return "signup";
     }
 }
--- src/main/java/gift/service/UserService.java
+++ src/main/java/gift/service/UserService.java
@@ -1,7 +1,10 @@
 package gift.service;
 
 import gift.mapper.UserMapper;
+import gift.model.User;
 import org.springframework.stereotype.Service;
+import java.security.SecureRandom;
+import java.util.Base64;
 
 @Service
 public class UserService {
@@ -12,5 +15,22 @@ public class UserService {
     public UserService(UserMapper userMapper, HashService hashService) {
         this.userMapper = userMapper;
         this.hashService = hashService;
+    }
+
+    public boolean isUsernameAvailable(String username) {
+        return userMapper.getUser(username) == null;
+    }
+
+    public int createUser(User user) {
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+        String encodedSalt = Base64.getEncoder().encodeToString(salt);
+        String hashedPassword = hashService.getHashedValue(user.getPassword(), encodedSalt);
+        return userMapper.insert(new User(null, user.getUsername(), encodedSalt, hashedPassword, user.getFirstName(), user.getLastName()));
+    }
+
+    public User getUser(String username) {
+        return userMapper.getUser(username);
     }
 }
