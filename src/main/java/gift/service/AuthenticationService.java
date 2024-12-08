647f01423c9eb510ea9b989547c71298f60df9a1
HoryunLee <ghfusdl7201@gmail.com>
2024-12-08 21:50:17 +0900

    ADD:Authentication

--- src/main/java/gift/service/AuthenticationService.java
+++ src/main/java/gift/service/AuthenticationService.java
@@ -1,10 +1,14 @@
 package gift.service;
 
 import gift.mapper.UserMapper;
+import gift.model.User;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 public class AuthenticationService implements AuthenticationProvider {
@@ -16,14 +20,26 @@ public class AuthenticationService implements AuthenticationProvider {
         this.hashService = hashService;
     }
 
-
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        User user = userMapper.getUser(username);
+        if (user != null) {
+            String encodedSalt = user.getSalt();
+            String hashedPassword = hashService.getHashedValue(password, encodedSalt);
+            if (user.getPassword().equals(hashedPassword)) {
+                return new UsernamePasswordAuthenticationToken(username, password, new ArrayList<>());
+            }
+        }
+
         return null;
     }
 
     @Override
     public boolean supports(Class<?> authentication) {
-        return false;
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
     }
 }
+
